ae0a43c546215b3f54d0c008ee87a2d23239a2d9
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-04-21 14:09:13 -0300

    sepração de classes em modulos

---
+++ JavaScript/JS2/ContaCorrente.js
@@ -0,0 +1,24 @@
+export class ContaCorrente{
+
+    agencia
+    cliente
+
+    _saldo = 0
+
+    sacar(valor){
+        if(this._saldo < 0 || this._saldo < valor)return
+        
+        this._saldo -= valor
+        return valor
+    }
+    depositar(valor){
+        if(valor > 0){
+            this._saldo += valor
+        }
+    }
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor)
+        conta.depositar(valorSacado)
+    }
+
+}
--- JavaScript/JS2/index.js
+++ JavaScript/JS2/index.js
@@ -1,25 +1,5 @@
-class Cliente{
-    nome
-    cpf
-}
-
-class ContaCorrente{
-
-    agencia
-    #saldo = 0
-
-    sacar(valor){
-        if(this.#saldo < 0 || this.#saldo < valor)return
-        
-        this.#saldo -= valor
-        return this.#saldo
-    }
-    depositar(valor){
-        if(valor > 0){
-            this.#saldo += valor
-        }
-    }
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente()
 cliente1.nome = "Ricardo"
@@ -31,13 +11,21 @@ cliente2.cpf = 88822233309
 
 const ContaCorrenteRicardo = new ContaCorrente()
 ContaCorrenteRicardo.agencia = 1001
+ContaCorrenteRicardo.cliente = cliente1
+
+const conta2 = new ContaCorrente()
+conta2.agencia = 1002
+conta2.cliente = cliente2
 
 ContaCorrenteRicardo.depositar(300)
 ContaCorrenteRicardo.depositar(-400) //o metodo nao modifica com esse argumento passado
 
-const valorRestante = ContaCorrenteRicardo.sacar(60)
+const valorSacado = ContaCorrenteRicardo.sacar(60)
 
-console.log(valorSacado)
-console.log(ContaCorrenteRicardo)
+ContaCorrenteRicardo.transferir(100,conta2)
+
+console.log(ContaCorrenteRicardo, conta2)
+
+
 
 // console.log(cliente1, cliente2)
