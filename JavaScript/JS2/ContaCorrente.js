7193abbeb5ed078a6039767ffa1e70fd1a780b6c
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-04-21 15:49:53 -0300

    curso JS2  POO finalizado

--- JavaScript/JS2/ContaCorrente.js
+++ JavaScript/JS2/ContaCorrente.js
@@ -1,9 +1,29 @@
+import { Cliente } from "./Cliente.js"
 export class ContaCorrente{
 
     agencia
-    cliente
-
+    static numeroDeContas = 0
+    
+    _cliente
     _saldo = 0
+
+    set cliente(novoCliente){ //nao é chamado de metedo, e sim acessor
+        if(novoCliente instanceof Cliente)this._cliente = novoCliente
+    }
+    
+    get cliente(){
+        return this._cliente
+    }
+
+    get saldo(){
+        return this._saldo
+    }
+    constructor(cliente, agencia){
+        this.agencia = agencia
+        this.cliente = cliente //nao vai direto para _cliente, pois quero verificar antes através do acessor  set cliente
+        ContaCorrente.numeroDeContas++
+    }
+
 
     sacar(valor){
         if(this._saldo < 0 || this._saldo < valor)return
--- JavaScript/JS2/index.js
+++ JavaScript/JS2/index.js
@@ -1,30 +1,31 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente()
-cliente1.nome = "Ricardo"
-cliente1.cpf = 11122233309
+ const cliente1 = new Cliente("Ricardo", 11122233309) //aloca no heap o espaço de um Cliente
 
-const cliente2 = new Cliente()
-cliente2.nome = "Alice"
-cliente2.cpf = 88822233309
 
-const ContaCorrenteRicardo = new ContaCorrente()
-ContaCorrenteRicardo.agencia = 1001
-ContaCorrenteRicardo.cliente = cliente1
+// const cliente2 = new Cliente("Alice", 88822233309)
 
-const conta2 = new ContaCorrente()
-conta2.agencia = 1002
-conta2.cliente = cliente2
+ const ContaCorrenteRicardo = new ContaCorrente(cliente1, 1001)
 
-ContaCorrenteRicardo.depositar(300)
-ContaCorrenteRicardo.depositar(-400) //o metodo nao modifica com esse argumento passado
 
-const valorSacado = ContaCorrenteRicardo.sacar(60)
+// const conta2 = new ContaCorrente(cliente2, 1002) 
 
-ContaCorrenteRicardo.transferir(100,conta2)
 
-console.log(ContaCorrenteRicardo, conta2)
+
+
+
+
+//ContaCorrenteRicardo.depositar(300)
+//ContaCorrenteRicardo.depositar(-400) //o metodo nao modifica com esse argumento passado
+
+//const valorSacado = ContaCorrenteRicardo.sacar(60)
+
+//ContaCorrenteRicardo.transferir(100,conta2)
+
+//console.log(conta2, ContaCorrenteRicardo)
+
+console.log(ContaCorrente.numeroDeContas)
 
 
 
