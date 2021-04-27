f7de46a7154f404097ac117dc052cb864cd0d7d0
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-04-27 20:33:15 -0300

    continuação do curso de java ( condicioanais)

---
+++ Java/TestaCaracteres.java
@@ -0,0 +1,23 @@
+public class TestaCaracteres{
+
+	public static void main(String args[]){
+		
+		char letra = 'a';
+		
+		System.out.println(letra);
+		
+		letra =  (char) (letra + 1);
+		
+		System.out.println(letra);
+		
+		String palavra = "Alex estudando java sla";
+		
+		System.out.println(palavra);
+		
+		palavra = palavra + 2020;
+		
+		System.out.println(palavra);
+		
+	}
+	
+}
