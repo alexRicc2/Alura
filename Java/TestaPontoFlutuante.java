2acda58ee5a25309ad0d89ef380f899870b48bbd
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-04-27 20:00:10 -0300

    Iniciando curso de java da alura

---
+++ Java/TestaPontoFlutuante.java
@@ -0,0 +1,8 @@
+public class TestaPontoFlutuante {
+	
+	public static void main(String args[]){
+		double salario = 1250.70;
+		System.out.println("meu salario é " + salario);
+		System.out.println("divisao de um double por int faz corretamente: " + 5.0 / 2);
+	}
+}
