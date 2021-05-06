7ee74498a4396df090114f73f2da8a9dec4ebcee
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-05-06 17:02:36 -0300

    1 aula js-web2

--- JS-web/main.js
+++ JS-web/main.js
@@ -3,28 +3,47 @@ import BotaoDeleta from './componentes/deletaTarefa.js'
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-function criarTarefa(evento){
-    evento.preventDefault()
-    
-    const lista = document.querySelector('[data-list]')
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-    
-    const input = document.querySelector('[data-form-input]')
+const handleNovoItem = (evento) => {
 
+    evento.preventDefault()
+
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    console.log(valor)
+
+    const calendario = document.querySelector('[data-form-date]')
+    const data = moment(calendario.value)
+    const dataFormatada = data.format('DD/MM/YYYY')
+
+    const dados = {
+        valor, dataFormatada
+    }
+
+    const criaTarefa = criarTarefa(dados)
+
+    lista.appendChild(criaTarefa)
+
+    sessionStorage.setItem('tarefas' , dados)
     input.value = " "
 
-    const conteudo = `<p class="content">${valor}</p>`
+}
+
+
+function criarTarefa({ valor, dataFormatada}){
+    
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+
+    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
 
     tarefa.innerHTML = conteudo
     tarefa.appendChild(botaoConcluir())
     tarefa.appendChild(BotaoDeleta())
-    lista.appendChild(tarefa)
+
+    return tarefa
 }
 
-novaTarefa.addEventListener('click', criarTarefa)
+novaTarefa.addEventListener('click', handleNovoItem)
 
 
 
