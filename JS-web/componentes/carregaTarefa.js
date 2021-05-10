c6a8a2e519a3829f6ed4dc81852a1aff7625e5d7
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-05-10 19:41:25 -0300

    js web2

---
+++ JS-web/componentes/carregaTarefa.js
@@ -0,0 +1,10 @@
+export const carregaTarefa = () =>{
+
+    const lista = document.querySelector('[data-list]')
+
+    const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas')) || []
+
+    tarefasCadastradas.forEach((tarefa) =>{
+        lista.appendChild(Tarefa(tarefa))
+    })
+}
--- JS-web/main.js
+++ JS-web/main.js
@@ -1,49 +1,11 @@
-import botaoConcluir from './componentes/concluiTarefa.js'
-import BotaoDeleta from './componentes/deletaTarefa.js'
+
+import {handleNovoItem} from './componentes/criaTarefas.js'
+import {carregaTarefa} from './componentes/carregaTarefa.js'
 
 const novaTarefa = document.querySelector('[data-form-button]')
 
-const handleNovoItem = (evento) => {
-
-    evento.preventDefault()
-
-    const lista = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-input]')
-    const valor = input.value
-
-    const calendario = document.querySelector('[data-form-date]')
-    const data = moment(calendario.value)
-    const dataFormatada = data.format('DD/MM/YYYY')
-
-    const dados = {
-        valor, dataFormatada
-    }
-
-    const criaTarefa = criarTarefa(dados)
-
-    lista.appendChild(criaTarefa)
-
-    sessionStorage.setItem('tarefas' , dados)
-    input.value = " "
-
-}
-
-
-function criarTarefa({ valor, dataFormatada}){
-    
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
-
-    const conteudo = `<p class="content">${dataFormatada} * ${valor}</p>`
-
-    tarefa.innerHTML = conteudo
-    tarefa.appendChild(botaoConcluir())
-    tarefa.appendChild(BotaoDeleta())
-
-    return tarefa
-}
-
 novaTarefa.addEventListener('click', handleNovoItem)
 
+carregaTarefa()
 
 
