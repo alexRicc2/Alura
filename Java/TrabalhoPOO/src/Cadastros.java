02c1298a481460d1fbd582c9d5decd92ebab1bf8
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-06-04 21:57:51 -0300

    novo sistema

--- Java/TrabalhoPOO/src/Cadastros.java
+++ Java/TrabalhoPOO/src/Cadastros.java
@@ -1,68 +1,13 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Cadastros {
 
-	public static void main(String args[]) {
-		
-		int op = -1;
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Passageiro> passageirosLista = new ArrayList<>();
-		ArrayList<Rotas> rotasLista = new ArrayList<>();
-		ArrayList<Onibus> onibusLista = new ArrayList<>();
-		ArrayList<Motorista> motoristaLista = new ArrayList<>();
-		
-		while(op != 0) {
-		System.out.println("Sistema de cadastros");
-		System.out.println("(1)Passageiro");
-		System.out.println("(2)Rota");
-		System.out.println("(3)Cadastrar um Onibus");
-		System.out.println("(4)Cadastrar um Motorista");
-		System.out.println("(0)Sair do Sistema");
-		System.out.println("Sua escolha: ");
-		op = sc.nextInt();
-		
-		
-		if(op == 2) {
-			
-			System.out.println("(1)Cadastrar um rota");
-			System.out.println("(2)Alterar um rota");
-			System.out.println("(3)Excluir um rota");
-			System.out.println("Sua escolha: ");
-			
-			op = sc.nextInt();
-			
-			if(op == 1) {
-				int cod,ci;
-			
-				
-				ci = sc.nextInt();
-				
-				Rotas rota = new Rotas(ci,2);
-				rota.mostraCidade(ci);
-			}
-			
-		}
-		if(op == 3) {
+		public Onibus cadastraOnibus(Motorista m1) {
+			Scanner sc = new Scanner(System.in);
 			String modelo,marca;
 			int anoF;
 			double kilometros;
-			int i;
-			
-			System.out.println("(1)Cadastrar um Onibus");
-			System.out.println("(2)Alterar um Onibus");
-			System.out.println("(3)Excluir um Onibus");
-			System.out.println("Sua escolha: ");
-			
-			op = sc.nextInt();
-			
-			if(op == 1) {
-				
-				if(motoristaLista.isEmpty()) {
-					System.out.println("Sem motorista pre cadastrados");
-				}
-				else {
-				
+		
 				System.out.println("Modelo do onibus: ");
 				modelo = sc.next();
 				
@@ -75,46 +20,43 @@ public class Cadastros {
 				System.out.println("Kilometragem do Onibus: ");
 				kilometros = sc.nextDouble();
 				
-				
-				for(i=0;i<motoristaLista.size();i++) {
-					System.out.println("Motorista " + i + " " + motoristaLista.get(i).getNome());
-				}
-				System.out.println("Numero do motorista para o Onibus: ");
-				i = sc.nextInt();
-				
-				
-				Onibus o1 = new Onibus(modelo,anoF,marca,kilometros,motoristaLista.get(i));
-				onibusLista.add(o1);
+				Onibus o1 = new Onibus(modelo,anoF,marca,kilometros,m1);
 				
 				System.out.println("Onibus de modelo " + o1.getModelo()+ " Cadastrado!");
-				
-				}
-			}
-			else if(op == 3) {
-				
-				System.out.println("Excluindo um Onibus");
-				for(i=0;i<onibusLista.size();i++) {
-					System.out.println("Onibus " + i + " " + onibusLista.get(i).getModelo());
-				}
-				System.out.println("Numero do onibus para ser excluido: ");
-				i = sc.nextInt();
-				
-				onibusLista.remove(i);
-				
-				System.out.println("Onibus removido");
-				
-				
-				
-			}
+				return o1;
 		}
-		
-		if(op == 4) {
+		public Onibus alteraOnibus(Onibus o1) {
+			int i;
+			Scanner sc = new Scanner(System.in);
 			
-			System.out.println("(1)Cadastrar um motorista");
+			System.out.println("Alterando onibus");
+			
+			System.out.println("(1)Mudar modelo- UNICO POSSIVEL NO MOMENTO");
+			System.out.println("(2)Mudar ano de fabricacao");
+			System.out.println("(3)Mudar marca");
+			System.out.println("(4)Mudar kilometragem");
+			i = sc.nextInt();
+			
+			if(i == 1) {
+				String modelo;
+				System.out.println("Novo modelo: ");
+				modelo = sc.next();
+				o1.setModelo(modelo);
+				
+			}
+			
+			
+			return o1;
+			
+		}
+		public Motorista cadastraMotorista() {
 			
 			String nome;
 			double Cnh;
 			int ano,mes,dia,hora,minuto;
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("(1)Cadastrar um motorista");
 			
 			System.out.println("Digite o nome do motorista: ");
 			nome = sc.next();
@@ -131,11 +73,10 @@ public class Cadastros {
 			
 			Data data1 = new Data(dia,mes,ano);
 			Motorista m1 = new Motorista(Cnh,data1,nome);
-			motoristaLista.add(m1);
 			
 			System.out.println("O motorista "+ m1.getNome() + " Foi cadastrado com sucesso");
 			System.out.println("O motorista tem data de admissao em "+ m1.getDataAdimissao().getData());
-		}
-		}
+			
+			return m1;
 		}
 	}
---
+++ Java/TrabalhoPOO/src/Principal.java
@@ -0,0 +1,126 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Principal {
+
+public static void main(String args[]) {
+		
+		Cadastros sistemaC = new Cadastros();
+		
+		
+		int op = -1,decisao = -1, opSistemas = -1;
+		
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Passageiro> passageirosLista = new ArrayList<>();
+		ArrayList<Rotas> rotasLista = new ArrayList<>();
+		ArrayList<Onibus> onibusLista = new ArrayList<>();
+		ArrayList<Motorista> motoristaLista = new ArrayList<>();
+		
+		while(opSistemas != 0) {
+		
+			System.out.println("1 - Sistema de Cadastros");
+			System.out.println("2 - Mostrar cadastros");
+			System.out.println("3 - Comprar Passagens");
+			opSistemas = sc.nextInt();
+			
+			if(opSistemas == 1) {
+				
+				while(op != 0) {
+					System.out.println("Sistema de cadastros");
+					System.out.println("(1)Passageiro");
+					System.out.println("(2)Rota");
+					System.out.println("(3)Cadastrar um Onibus");
+					System.out.println("(4)Cadastrar um Motorista");
+					System.out.println("(0)Sair do Sistema de cadastros");
+					System.out.println("Sua escolha: ");
+					op = sc.nextInt();
+					
+					
+					if(op == 3) {
+						int i;
+						
+						System.out.println("(1)Cadastrar um Onibus");
+						System.out.println("(2)Alterar um Onibus");
+						System.out.println("(3)Excluir um Onibus");
+						System.out.println("Sua escolha: ");
+						
+						decisao = sc.nextInt();
+						
+						if(decisao == 1) { //cadastra onibus
+							
+							if(motoristaLista.isEmpty()) {
+								System.out.println("Sem motorista pre cadastrados");
+							}
+							else {
+								
+								for(i=0;i<motoristaLista.size();i++) {
+									System.out.println("Motorista " + i + " " + motoristaLista.get(i).getNome());
+								}
+								System.out.println("Numero do motorista para o Onibus: ");
+								i = sc.nextInt();
+								
+								Onibus o1 = sistemaC.cadastraOnibus(motoristaLista.get(i));
+								onibusLista.add(o1);
+							}
+						}
+						if(decisao == 2) {
+							for(i=0;i<onibusLista.size();i++) {
+								System.out.println("onibus " + i + " " + onibusLista.get(i).getModelo());
+							}
+							System.out.println("Numero do Onibus a ser alterado: ");
+							i = sc.nextInt();
+							
+							Onibus o = sistemaC.alteraOnibus(onibusLista.get(i));
+							onibusLista.remove(i);
+							onibusLista.add(o);
+						}
+						else if(decisao == 3) { //deleta onibus
+							
+							System.out.println("Excluindo um Onibus");
+							for(i=0;i<onibusLista.size();i++) {
+								System.out.println("Onibus " + i + " " + onibusLista.get(i).getModelo());
+							}
+							System.out.println("Numero do onibus para ser excluido: ");
+							i = sc.nextInt();
+							
+							onibusLista.remove(i);
+							
+							System.out.println("Onibus removido");
+								
+						}
+					}
+					
+					if(op == 4) { //cadastra motorista
+						
+						Motorista m1 = sistemaC.cadastraMotorista();
+						motoristaLista.add(m1);
+					}
+					
+					}
+				}
+			if(opSistemas == 2) {
+				System.out.println("1- Lista de passageiros");
+				System.out.println("2- Lista de rotas");
+				System.out.println("3- Lista de onibus");
+				System.out.println("4- Lista de motorista");
+				op = sc.nextInt();
+				
+				if(op == 3) {
+					int i;
+					System.out.println("Ha "+ onibusLista.size() +" onibus cadastrados");
+					for(i=0;i<onibusLista.size();i++) {
+						System.out.println(i+1 + "- "+ onibusLista.get(i).getModelo());
+					}
+				}
+				
+				if(op == 4) {
+					int i;
+					System.out.println("Ha "+ motoristaLista.size() +" motoristas cadastrados");
+					for(i=0;i<motoristaLista.size();i++) {
+						System.out.println(i+1 + "- "+ motoristaLista.get(i).getNome());
+					}
+				}
+			}
+			}
+	}
+}
