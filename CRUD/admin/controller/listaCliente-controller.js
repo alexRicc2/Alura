0ed8e69f42b5e94392e75db2bace2526e663c228
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-06-08 01:49:05 -0300

    crud

---
+++ CRUD/admin/controller/listaCliente-controller.js
@@ -0,0 +1,24 @@
+import {clienteService} from '../service/cliente-service'
+
+const criaNovaLinha = (nome, email,id) => {
+    const linhaNovoCliente = document.createElement('tr')
+
+    const conteudo = `
+<td class="td" data-td>${nome}</td>
+                <td>${email}</td>
+                <td>
+                    <ul class="tabela__botoes-controle">
+                        <li><a href="../telas/edita_cliente.html?id=${id}" class="botao-simples botao-simples--editar">Editar</a></li>
+                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
+                    </ul>
+                </td>`
+                linhaNovoCliente.innerHTML = conteudo
+                return linhaNovoCliente
+}
+
+const tabela = document.querySelector('[data-tabela]')
+clienteService.listaClientes()
+.then(data => {
+        data.forEach(elemento => {
+        tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email))
+})})
