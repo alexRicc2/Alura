c33ef951f9aa76bbeacf65acd9790bf66b32f639
Alex Ricardo Rodrigues Santanna <alex.65.santanna@gmail.com>
2021-06-29 16:45:43 -0300

    deleta card reactApp

--- ReactApp/ceep/src/App.js
+++ ReactApp/ceep/src/App.js
@@ -20,12 +20,17 @@ class App extends Component {
     }
     this.setState(novoEstado)
   }
+  deletarNota(index){
+    let arrayNotas = this.state.notas;
+    arrayNotas.splice(index,1);
+    this.setState({notas: arrayNotas});
+  }
 
   render() {
     return (
       <section className="conteudo">
         <FormularioCadastro criarNota={this.criarNota.bind(this)}/>
-        <ListaDeNotas notas={this.state.notas}/>
+        <ListaDeNotas notas={this.state.notas} apagarNota={this.deletarNota.bind(this)}/>
       </section>
     );
   }
--- ReactApp/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
+++ ReactApp/ceep/src/components/ListaDeNotas/ListaDeNotas.jsx
@@ -8,7 +8,8 @@ class ListaDeNotas extends Component {
         {this.props.notas.map((nota, index) => {
           return (
             <li className="lista-notas_item" key={index}>
-              <CardNota titulo={nota.titulo} texto={nota.texto}/>
+              <CardNota titulo={nota.titulo} texto={nota.texto}
+              indice={index} apagarNota={this.props.apagarNota}/>
             </li>
           );
         })}
